c425dd936f44d89d84ec913e799478c4fc4933aa
Quim Nuss Székely <quimnuss@altaveu>
2025-07-13 22:51:46 +0200

    fix hard-coded scene paths

--- scripts/world/dungeon_test_layout.gd
+++ scripts/world/dungeon_test_layout.gd
@@ -3,6 +3,6 @@ extends Node3D
 @export_enum ('dungeon_start', 'dungeon_room_1') var dungeon : String = 'dungeon_start'
 
 func _ready():
-	var path : String = 'res://dungeons/' + dungeon + '.tscn'
+	var path : String = 'res://levels/dungeons/' + dungeon + '.tscn'
 	var dungeon_scene = load(path).instantiate()
 	add_child(dungeon_scene)
--- scripts/world/house_room.gd
+++ scripts/world/house_room.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 func to_dungeon():
-	get_tree().change_scene_to_file("res://world/world.tscn")
+	get_tree().change_scene_to_file("res://levels/world.tscn")
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
