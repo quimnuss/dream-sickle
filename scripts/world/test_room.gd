a8211bab9c4474444ba13336deaac38002ae6487
Quim Nuss Székely <quimnuss@altaveu>
2025-07-16 22:45:59 +0200

    roll and momentum logic

--- scripts/world/test_room.gd
+++ scripts/world/test_room.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@export_enum ('dungeon_start', 'dungeon_room_1') var dungeon : String = 'dungeon_start'
+@export_enum ('dungeon_start', 'dungeon_room_1', 'dungeon_room_2', 'test_playground', 'test_iwork') var dungeon : String = 'dungeon_start'
 
 func _ready():
-	var path : String = 'res://levels/dungeons/' + dungeon + '.tscn'
+	var path : String = 'res://levels/dungeons/' + dungeon + '.tscn' if not dungeon.begins_with('test_') else 'res://levels/test_rooms/' + dungeon + '.tscn'
 	var dungeon_scene = load(path).instantiate()
 	add_child(dungeon_scene)
